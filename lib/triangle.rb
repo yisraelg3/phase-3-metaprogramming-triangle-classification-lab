833cb229a6b204843b9f54ab024b893cc31b4900
Yisrael Gurkow <yisrelg3@gmail.com>
2021-06-15 01:01:58 -0400

    commit tree a5e561be895dc616a9f3fdc56477964576fadb46

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_reader :one, :two, :three
+  def initialize(one, two ,three)
+    @one = one
+    @two = two
+    @three = three
+  end
+
+  def kind
+    validate_triangle
+    if @one == @two && @two == @three
+      :equilateral
+    elsif @one == @two || @two == @three || @three == @one
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    if @one + @two <= @three || @two + @three <= @one || @one + @three <= @two || @one + @two + @three <= 0
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    # def message
+    #   "illegal triangle"
+    # end
+  end
 end
